2348f36da32f284e5dacfeafc15b75a4a406232f
mguenther006 <mguenther006@users.noreply.github.com>
2015-08-10 15:37:19 -0600

    updated nodeTest and added textSupport folder

--- ToyBox/toybox.js
+++ ToyBox/toybox.js
@@ -243,3 +243,120 @@ var fibCheck = function(n) {
 };
 
 console.log(fibCheck(7));
+
+//New problem below
+
+var animals = [
+    {
+        name: 'Dog',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Mammalia',
+        order: 'Carnivora',
+        family: 'Canidae',
+        species: 'Canis lupus'
+    },
+    {
+        name: 'Cat',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Mammalia',
+        order: 'Carnivora',
+        family: 'Felidae',
+        species: 'Felis catus'
+    },
+    {
+        name: 'Hippopotamus',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Mammalia',
+        order: 'Artiodactyla',
+        family: 'Hippopotamidae',
+        species: 'Hippopotamus amphibius'
+    },
+    {
+        name: 'Playtpus',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Mammalia',
+        order: 'Monotremata',
+        family: 'Ornithorhynchidae',
+        species: 'Ornithorhynchus anatinus'
+    },
+    {
+        name: 'Lizard',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Reptilia',
+        order: 'Squamata',
+        family: 'Agamidae',
+        species: 'Pogona vitticeps'
+    },
+    {
+        name: 'Blackbird',
+        kingdom: 'Animalia',
+        phylum: 'Chordata',
+        class: 'Aves',
+        order: 'Passeriformes',
+        family: 'Turdidae',
+        species: 'Turdus merula'
+    }
+];
+
+//given the above object, write a function that returns an object whose keys are the fields found above and whose values are objects containing all the values as keys and a count for how many times that value occurs
+
+//e.g.
+//{
+//    kingdom: {Animalia: 6}
+//    phylum: {Chordata: 6},
+//    class: {Mammalia: 4, Reptilia: 1, Aves: 1}
+//}
+
+//code here
+
+//SOLUTION 1
+var group = function(animals) {
+    var results = {
+        name: {},
+        kingdom: {},
+        phylum: {},
+        class: {},
+        order: {},
+        family: {},
+        species: {}
+    };
+    for (var i = 0; i < animals.length; i++) {
+        for (var property in animals[i]) {
+            if (!results[property].hasOwnProperty(animals[i][property])) {
+                results[property][animals[i][property]] = 1;
+            } else {
+                results[property][animals[i][property]] += 1;
+            }
+        }
+    }
+    return results;
+};
+
+group(animals);
+
+//SOLUTION 2 - MIHIR
+
+var countValue = function(items) {
+    var b = {};
+    for(var i= 0; i<items.length; i++) {
+        for(var prop in items[i]) {
+            if(!b[prop]) {
+                b[prop] = {};
+            }
+            if(!b[prop][items[i][prop]]) {
+                b[prop][items[i][prop]] = 1;
+            }
+            else {
+                b[prop][items[i][prop]]++;
+            }
+        }
+    }
+    return b;
+}
+
+console.log(countValue(animals));
