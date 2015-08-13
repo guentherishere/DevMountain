5cc6c55ca60f3d71269531a52f7be18b9d981e1e
mguenther006 <mguenther006@users.noreply.github.com>
2015-08-12 19:08:07 -0600

    included new controller. edited routes and get, post, put, delete

--- Week6/Day2-Mongo/eCommerce/public/controllers/adminCtrl.js
+++ Week6/Day2-Mongo/eCommerce/public/controllers/adminCtrl.js
@@ -2,23 +2,26 @@ app.controller('adminCtrl', function($scope, storeService) {
 
   var itemGrabber = function() {
     storeService.getItems().then(function(response) {
-      $scope.products = data;
+      $scope.products = response;
     });
   };
   itemGrabber();
+
   $scope.addNewProduct = function() {
-    storeService.addProductData($scope.product).then(function(data) {
+    storeService.addProductData($scope.product).then(function(response) {
       itemGrabber();
     });
   };
+
   $scope.deleteProduct = function(product) {
     console.log(product);
-    storeService.deleteProductData(product).then(function(data) {
+    storeService.deleteProductData(product).then(function(response) {
       itemGrabber();
     });
   };
+
   $scope.updateProduct = function(product) {
-    storeService.updateProductData(product).then(function(data) {
+    storeService.updateProductData(product).then(function(response) {
       itemGrabber();
     });
   };
--- Week6/Day2-Mongo/eCommerce/public/services/storeService.js
+++ Week6/Day2-Mongo/eCommerce/public/services/storeService.js
@@ -1,5 +1,3 @@
-// 'use strict';
-
 app.service('storeService', function($q, $http) {
 
   this.getItems = function() {
@@ -10,6 +8,49 @@ app.service('storeService', function($q, $http) {
     }).then(function(response) {
       var parsedResponse = response.data;
       deferred.resolve(parsedResponse);
+    });
+    return deferred.promise;
+  };
+
+  this.addProductData = function(product) {
+    var deferred = $q.defer();
+    $http({
+      method: 'POST',
+      url: 'http://localhost:1336/api/store',
+      data: {
+        title: product.title,
+        description: product.description,
+        price: product.price
+      }
+    }).then(function(response) {
+      deferred.resolve(response);
+    });
+    return deferred.promise;
+  };
+
+  this.deleteProductData = function(product) {
+    var deferred = $q.defer();
+    $http({
+      method: 'DELETE',
+      url: 'http://localhost:1336/api/store' + product._id
+    }).then(function(response) {
+      deferred.resolve(response);
+    });
+    return deferred.promise;
+  };
+
+  this.updateProductData = function(product) {
+    var deferred = $q.defer();
+    $http({
+      method: 'PUT',
+      url: 'http://localhost:1336/api/store' + product._id,
+      data: {
+        title: product.title,
+        description: product.description,
+        price: product.price
+      }
+    }).then(function(response) {
+      deferred.resolve(response);
     });
     return deferred.promise;
   };
